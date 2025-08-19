d989709520a045f339caf0f35a1b09188ceb2273
patman <27874300+patmanf@users.noreply.github.com>
2025-08-19 00:29:11 -0500

    rewrite like  the entire thing

--- pat/ruler/ruler.lua
+++ pat/ruler/ruler.lua
@@ -1,186 +1,211 @@
-require "/scripts/util.lua"
 require "/scripts/vec2.lua"
-require "/scripts/messageutil.lua"
 
-local oldInit = init or function() end
-local oldUpdate = update or function() end
+local Ruler = {}
+pat_ruler = Ruler
 
-local drawRuler, rulerEnabled, gridEnabled, aimPosition
+function Ruler:init()
+  if not storage.pat_ruler then storage.pat_ruler = {} end
+  self.storage = setmetatable(storage.pat_ruler, nil)
 
-local function equipTech(t)
-	if not t then return end
-  player.makeTechAvailable(t)
-  player.enableTech(t)
-  player.equipTech(t)
-end
-
-local function toggleRuler()
-  rulerEnabled = not rulerEnabled
-  player.setProperty("pat_ruler_enabled", rulerEnabled)
-  return rulerEnabled
-end
-
-local function toggleGrid()
-  gridEnabled = not gridEnabled
-  player.setProperty("pat_tilegrid_enabled", gridEnabled)
-  return gridEnabled
-end
-
-
-function init()
-	oldInit()
-	
-	rulerEnabled = player.getProperty("pat_ruler_enabled", false)
-	gridEnabled = player.getProperty("pat_tilegrid_enabled", false)
-	
-  --the toggler handling
-	message.setHandler("pat_ruler_toggle", localHandler(toggleRuler))
-	message.setHandler("pat_tilegrid_toggle", localHandler(toggleGrid))
+  self.config = root.assetJson("/pat/ruler/ruler.config")
   
-  --get aimposition
-	if starExtensions then
-    aimPosition = player.aimPosition
-  else
-    local m = getmetatable''
-    m.pat_ruler = {}
-    m.pat_ruler.done = function()
-      aimPosition = m.pat_ruler.aimPosition
-      equipTech(player.getProperty("pat_rulerLastTech"))
-      player.makeTechUnavailable("pat_ruler_head")
-      m.pat_ruler = nil
+  self.characters = {}
+  for _, char in pairs(self.config.charFrames) do
+    local file = sb.replaceTags(self.config.charImage, {frame = char})
+    local size = root.imageSize(file)
+    self.characters[char] = { file = file, width = size[1] }
+  end
+  
+  self.techSlot = root.techType(self.config.techName)
+  self:restoreHeadTech()
+  for _, tech in pairs(self.config.removedTechs) do
+    player.makeTechUnavailable(tech)
+  end
+
+  self.lastPos = entity.position()
+  self.fade = 1
+  
+  self.getAimPosition = player.aimPosition -- SE/OSB add this yayy :3
+  if not self.getAimPosition then self:smuggleAimPosition() end
+  
+  self.checkBinds = input and input.bindDown ~= nil
+  self.scaleText = camera and interface and camera.pixelRatio ~= nil and interface.scale ~= nil
+
+  local function setHandler(n, f)
+    message.setHandler(n, function(_, L, ...) if L then return f(self, ...) end end)
+  end
+  setHandler("pat_ruler_toggle", self.toggleRuler)
+  setHandler("pat_tilegrid_toggle", self.toggleGrid)
+end
+
+function Ruler:equipTech(tech)
+  if not tech then return end
+  player.makeTechAvailable(tech)
+  player.enableTech(tech)
+  player.equipTech(tech)
+end
+
+function Ruler:restoreHeadTech()
+  if player.equippedTech(self.techSlot) == self.config.techName then
+    player.makeTechUnavailable(self.config.techName)
+    self:equipTech(self.storage.lastTech)
+    self.storage.lastTech = nil
+  end
+end
+
+function Ruler:toggleRuler()
+  self.storage.rulerEnabled = not self.storage.rulerEnabled
+  return self.storage.rulerEnabled
+end
+
+function Ruler:toggleGrid()
+  self.storage.gridEnabled = not self.storage.gridEnabled
+  return self.storage.gridEnabled
+end
+
+function Ruler:smuggleAimPosition()
+  self.storage.lastTech = player.equippedTech(self.techSlot)
+
+  local mt = getmetatable''
+  mt.pat_ruler_smuggleAimPosition = function(getAimPosition)
+    mt.pat_ruler_smuggleAimPosition = nil
+    self.getAimPosition = getAimPosition
+    self:restoreHeadTech()
+  end
+
+  self:equipTech(self.config.techName)
+end
+
+function Ruler:update(dt)
+  if not self.getAimPosition then return end
+  
+  if self.checkBinds then
+    if input.bindDown("pat_ruler", "toggleRuler") then self:toggleRuler() end
+    if input.bindDown("pat_ruler", "toggleGrid") then self:toggleGrid() end
+  end
+
+  if not self.storage.rulerEnabled and not self.storage.gridEnabled then return end
+
+  self.dt = dt
+  self.aimPos = self.getAimPosition()
+  self.plrPos = entity.position()
+  self.aimTile = { math.floor(self.aimPos[1]), math.floor(self.aimPos[2]) }
+  self.plrTile = { math.floor(self.plrPos[1]), math.floor(self.plrPos[2]) }
+
+  local moved = world.magnitude(self.plrPos, self.lastPos)
+  if moved > self.config.fadeMovement then
+    if self.fade > self.config.fadeMinimum then
+      self.fade = math.max(self.config.fadeMinimum, self.fade - self.dt / self.config.fadeTime)
     end
-    
-    local head = player.equippedTech("head")
-    if head ~= "pat_ruler_head" then
-      player.setProperty("pat_rulerLastTech", head)
+  elseif self.fade < 1 then
+    self.fade = math.min(1, self.fade + self.dt / self.config.fadeTime)
+  end
+
+  if self.storage.rulerEnabled then self:drawRuler() end
+  if self.storage.gridEnabled then self:drawGrid() end
+
+  self.lastPos = self.plrPos
+end
+
+function Ruler:drawRuler()
+  local startTile = {self.plrTile[1], self.plrTile[2] - 2}
+  local dist = world.distance(self.aimTile, startTile)
+  local x, y = dist[1], dist[2]
+  if x > 0 then startTile[1] = startTile[1] + 1 end
+  if x == 0 then x = x + 1 end
+  if y >= 0 then y = y + 1 end
+
+  local xDir = x < 0 and -1 or 1
+  local yDir = y < 0 and -1 or 1
+
+  local layer = self.config.layer
+  local drawable = {}
+  drawable.fullbright = true
+  drawable.color = self.config.color
+  drawable.color[4] = self.config.polyAlpha * self.fade
+  drawable.position = world.distance(startTile, self.plrPos)
+  
+  local poly = { {x, 1}, {x, 0}, {0, 0}, {0, 1} }
+  drawable.poly = poly
+  
+  if y ~= 0 then
+    if y <= 0 then
+      poly[1], poly[2] = poly[2], poly[1]
+      poly[3], poly[4] = poly[4], poly[3]
     end
-    equipTech("pat_ruler_head")
-  end
-	
-	--remove very old techs
-	for _,t in ipairs(player.availableTechs()) do
-		if t == "pat_ruler_body" or t == "pat_ruler_legs" then
-			player.makeTechUnavailable(t)
-		end
-	end
-end
 
-
-function update(dt)
-	oldUpdate(dt)
-  
-  if not aimPosition then return end
-	
-  --starextensions binds
-  if starExtensions then
-    if input.bindDown("pat_ruler", "toggleRuler") then toggleRuler() end
-    if input.bindDown("pat_ruler", "toggleGrid") then toggleGrid() end
-  end
-  
-  if not rulerEnabled and not gridEnabled then return end
-  
-  local epos = entity.position()
-  local eposCenter = util.tileCenter(epos)
-  local eDist = world.distance(eposCenter, epos)
-  local aim = util.tileCenter(aimPosition())
-  local d = world.distance(aim, eposCenter)
-  
-  if gridEnabled then
-    local pos = vec2.add(d, eDist)
-    localAnimator.addDrawable({image = "/pat/ruler/images/grid.png", fullbright = true, scale = 0.5, position = pos}, "Overlay+101")
-  end
-  
-  if rulerEnabled then
-    drawRuler(eposCenter, eDist, aim, d)
-  end
-end
-
--- the fuck
-drawRuler = function(eposCenter, eDist, aim, d)
-  local position = vec2.add(eDist, {0, -2})
-  aim[2] = aim[2] + 2
-  d[2] = d[2] + 2
-  
-  --what
-  if d[1] == 0 and d[2] ~= 0 then
-    if d[2] > 0 then
-      position[2] = position[2] - 1
-      d[2] = d[2] + 1
+    local x2 = x - xDir
+    if math.abs(x) == 1 then
+      poly[1], poly[4] = {x, y}, {x2, y}
     else
-      position[2] = position[2] + 1
-      d[2] = d[2] - 1
+      local y2 = y > 0 and 1 or 0
+      poly[5], poly[6], poly[7] = {x2, y2}, {x2, y}, {x, y}
     end
   end
   
-  local xOffset = (d[1] < 0 and -0.5 or 0.5)
-  local yOffset = (d[2] < 0 and -0.5 or 0.5)
+  localAnimator.addDrawable(drawable, layer)
   
-  local display = {math.abs(d[1]), math.abs(d[2])}
+  drawable.poly = nil
+  drawable.line = {}
+  drawable.width = self.config.lineWidth
+  drawable.color[4] = self.config.lineAlpha * self.fade
   
-  --generate poly
-  local poly1 = {
-    {xOffset, yOffset},
-    {xOffset, -yOffset},
-    {d[1] + xOffset, -yOffset},
-    {d[1] + xOffset, yOffset}
-  }
-  local poly2 = {
-    {d[1] + xOffset, yOffset},
-    {d[1] + xOffset, d[2] + yOffset},
-    {d[1] - xOffset, d[2] + yOffset},
-    {d[1] - xOffset, yOffset}
-  }
+  poly[0] = poly[#poly]
+  for i = 1, #poly do
+    drawable.line[1] = poly[i - 1]
+    drawable.line[2] = poly[i]
+    localAnimator.addDrawable(drawable, layer)
+  end
+
+  for i = 1, math.abs(x) - 1 do
+    local p = i * xDir
+    drawable.line[1] = {p, 0}
+    drawable.line[2] = {p, 1}
+    localAnimator.addDrawable(drawable, layer)
+  end
+
+  for i = (y > 0 and 1 or 0), math.abs(y) - 1 do
+    local p = i * yDir
+    drawable.line[1] = {x, p}
+    drawable.line[2] = {x - xDir, p}
+    localAnimator.addDrawable(drawable, layer)
+  end
   
-  if d[1] == 0 then
-    poly1 = {}
-    if d[2] == 0 then
-      poly2 = {{xOffset, yOffset}, {xOffset, -yOffset}, {-xOffset, -yOffset}, {-xOffset, yOffset}}
-      display[2] = 1
+  drawable.line, drawable.width = nil, nil
+  drawable.centered = false
+  drawable.color[4] = self.config.textAlpha * self.fade
+  drawable.scale = self.config.textScale
+
+  if self.scaleText then
+    drawable.scale = drawable.scale / camera.pixelRatio() * (interface.scale() + 1)
+  end
+  
+  local textPos = world.distance(self.aimPos, self.plrPos)
+  textPos = vec2.add(textPos, self.config.textOffset)
+  drawable.position = textPos
+
+  local str = string.format("%.0fx%.0f", math.abs(x), math.abs(dist[2]) + 1)
+  for char in str:gmatch(".") do
+    local cfg = self.characters[char]
+    if cfg then
+      drawable.image = cfg.file
+      localAnimator.addDrawable(drawable, layer)
+      textPos[1] = textPos[1] + (cfg.width * drawable.scale * 0.125)
     end
-    display[1] = 1
-  else
-    display[2] = display[2] + 1
   end
-  
-  --draw poly
-  localAnimator.addDrawable({poly = poly1, color = "#00EEFF44", fullbright = true, position = position}, "Overlay+100")
-  localAnimator.addDrawable({poly = poly2, color = "#00EEFF44", fullbright = true, position = position}, "Overlay+100")
-  
-  --draw lines
-  local fullPoly = copy(poly1)
-  for _,v in ipairs(poly2) do
-    table.insert(fullPoly, copy(v))
-  end
-  
-  for i = 1, #fullPoly do
-    local line = {fullPoly[i], fullPoly[i+1] or fullPoly[1]}
-    localAnimator.addDrawable({line = line, width = 1, color = "#00EEFF88", fullbright = true, position = position}, "Overlay+100")
-  end
-  
-  --draw more lines
-  local xMul = (d[1] < 0 and -1 or 1)
-  for i = 1, math.abs(d[1]) - 1 do
-    local x = i * xMul + xOffset
-    local line = {{x, -0.5}, {x, 0.5}}
-    localAnimator.addDrawable({line = line, width = 1, color = "#00EEFF88", fullbright = true, position = position}, "Overlay+100")
-  end
-  
-  local yMul = (d[2] < 0 and -1 or 1)
-  for i = 1, math.abs(d[2]) do
-    local y = (i - 1) * yMul + yOffset
-    local line = {{-0.5 + d[1], y}, {0.5 + d[1], y}}
-    localAnimator.addDrawable({line = line, width = 1, color = "#00EEFF88", fullbright = true, position = position}, "Overlay+100")
-  end
-  
-  --draw text
-  local textPos = vec2.add(world.distance(aim, eposCenter), {1.25, -0.75})
-  if d[2] < 0 then textPos[2] = textPos[2] - 2.5 end
-  
-  local str = string.format("%.0fx%.0f", display[1], display[2])
-  for i = 1, #str do
-    local h = str:sub(i, i)
-    localAnimator.addDrawable({image = "/pat/ruler/images/numbers.png:"..h, fullbright = true, scale = 0.75, position = textPos}, "Overlay+100")
-    
-    textPos[1] = textPos[1] + (h == "1" and 0.75 or 1.25) * 0.75
-	end
 end
+
+function Ruler:drawGrid()
+  local pos = world.distance(self.aimTile, self.plrPos)
+  pos[1], pos[2] = pos[1] + 0.5, pos[2] + 0.5
+  
+  local drawable = self.config.gridDrawable
+  drawable.position = pos
+  drawable.color[4] = self.config.gridAlpha * self.fade
+  
+  localAnimator.addDrawable(drawable, self.config.layer)
+end
+
+local _init, _update = init, update
+function init() _init() Ruler:init() end
+function update(dt) _update(dt) Ruler:update(dt) end
--- pat/ruler/tech/tech.lua
+++ pat/ruler/tech/tech.lua
@@ -1,5 +1,4 @@
 function init()
-  local m = getmetatable''
-  m.pat_ruler.aimPosition = tech.aimPosition
-  m.pat_ruler.done()
+  local mt = getmetatable''
+  if mt then pcall(mt.pat_ruler_smuggleAimPosition, tech.aimPosition) end
 end
