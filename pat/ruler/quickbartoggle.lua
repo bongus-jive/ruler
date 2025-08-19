1d0ab8d8d2eb7faeda273b544eacfaf16692568d
patman <27874300+patmanf@users.noreply.github.com>
2025-08-18 21:31:18 -0500

    better quickbar script

--- pat/ruler/quickbartoggle.lua
+++ pat/ruler/quickbartoggle.lua
@@ -1,11 +1,6 @@
-local msg = "pat_ruler_toggle"
-if params and params[1] == "grid" then
-	msg = "pat_tilegrid_toggle"
-end
-
-local enabled = world.sendEntityMessage(player.id(), msg):result()
-
-if enabled then
+local msg = params[1]
+local id = player.id()
+if msg and id ~= 0 and world.sendEntityMessage(id, msg):result() then
 	pane.playSound("/sfx/interface/nav_examine_on.ogg")
 else
 	pane.playSound("/sfx/interface/nav_examine_off.ogg")
